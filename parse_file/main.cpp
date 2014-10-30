c538332892b3eedf1529b15c26d2ab35e04ee7d1
= <matthew.giarra@gmail.com>
2014-10-30 18:28:23 -0400

    Able to read text files and return line matches. Next: parse and return the formatted values.

--- parse_file/main.cpp
+++ parse_file/main.cpp
@@ -7,31 +7,20 @@
 #define KBLU  "\x1B[34m"
 #define RESET "\033[0m"
 
-int main(int argc, char *argv[]){
-	
-	// Check number of input args
-	if(argc < 2){
-		printf(KRED "Error: Specify at least two inputs\n(missing file path and regexp string)\n" RESET);
-		return(-1);
-	}
-	
-	if(argc < 3){
-		printf(KRED "Error: Specify at least two inputs\n(missing regexp string)\n" RESET);
-		return(-1);
-	}
+void parse_file(std::string file_path, std::string string_to_match){
 		
-	// Read the input argument and assign it to the string variable "filename"
-	std::string filePath = argv[1];
+	/* To do:
+	(1) Specify with an input argument whether to return a string or a number(s).
 	
-	// Read argument as string to be matched
-	std::string string_to_match = argv[2];
+	(2) Accept a list of parameters to return?
 	
+	(3) Case insensitive regexp
+	
+	*/ 
+		
 	// Prefix / postfix for regexp
 	std::string pre = "\\b(";
 	std::string post = ")([^ ]*)";
-	
-	
-	// "\\b(_STRING_)([^ ]*)";
 	
 	// Combine the prefix + string to match + postfix
 	std::string reg_exp = pre + string_to_match + post;
@@ -47,13 +36,10 @@ int main(int argc, char *argv[]){
 	std::string line;
 	
 	// Open a file
-	std::ifstream myfile (filePath);
+	std::ifstream myfile (file_path);
 	
 	// Check to make sure the file was open, then read from it
 	if(myfile.is_open()){
-		
-		// Inform user that file was opened successfully.
-		printf(KBLU "Opened file!\n\n" RESET);
 		
 		// Declare line number
 		int line_num = 0;
@@ -61,26 +47,53 @@ int main(int argc, char *argv[]){
 		// Print each line of the file.
 		while(getline (myfile, line)){
 			if(std::regex_search(line, reg_exp_match, reg_exp_string)){
-				
+			
 				// Inform the user that the string was matched.
 				// Print the number of the line on which the match occurred.
 				printf(KBLU "String matched on line %d\n" RESET, line_num);
-				
+
 				// Print the whole line
-				std::cout << "Matched line: \"" << line << "\"\n";
+				printf("Line content: %s\n", line.c_str());
 			}
 			
 			// Increment line number
 			line_num++;
 		}
-		
 	}
+	
 	else{
 		printf(KRED "Error: Unable to open file!\n" RESET);
 	}
 	
 	// Close the file
-	myfile.close();
+	myfile.close();	
+}
+
+int main(int argc, char *argv[]){
+	
+	// Check number of input args
+	if(argc < 2){
+		printf(KRED "Error: Specify at least one inputs\n(missing file path)\n" RESET);
+		return(-1);
+	}
+	
+	// Read the input argument and assign it to the string variable "filename"
+	std::string file_path = argv[1];
+	
+	// Read argument as string to be matched
+	std::string string_to_match;
+
+	// Image width
+	string_to_match = "Image Width";
+	parse_file(file_path, string_to_match);
+	
+	// Image height
+	string_to_match = "Image Height";
+	parse_file(file_path, string_to_match);
+	
+	// Number of frames
+	string_to_match = "Original Total Frame";
+	parse_file(file_path, string_to_match);
 	
 	// GTFO
 	return(0);
