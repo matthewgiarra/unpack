7ec13afb8be7c475d4b6644fcccb0e12454d189d
Matthew Giarra <matthew.giarra@gmail.com>
2016-12-14 13:05:30 -0500

    Worked on jobfile mraw2tiff_jobfile_custom.py (added data cases that I plan to export)

--- mraw2tiff_jobfile_custom.py
+++ mraw2tiff_jobfile_custom.py
@@ -25,7 +25,7 @@ exec_path = './mraw2tiff'
 ######
 
 data_input_parent_dir = '/Volumes/duo/insects/analysis/data/xray_imaging/argonne_2015-06-05/argonne_2015-06-05_grasshoppers'
-data_output_parent_dir = '/Users/matthewgiarra/Desktop/argonne_2015-06-05_grasshoppers'
+data_output_parent_dir = '/Users/matthewgiarra/Documents/School/VT/Research/Aether/piv_test_images/grasshopper'
 
 
 # This is the number of the first image to export.
@@ -36,7 +36,7 @@ start_image = 0;
 # To export all the images in the MRAW file without
 # a-priori specifying the end image number,
 # set end_image = None
-end_image = 1000;
+end_image = None;
 
 # This is the bit-shift of the intensity values for the output images.
 # This shifts the position of the twelve image data bits in the MRAW data
@@ -46,7 +46,7 @@ end_image = 1000;
 # displayed visually on your system. Setting bit_shift to higher values
 # causes the images to appear brighter. A suggested default setting
 # is bit_shift = 3
-bit_shift = 4;
+bit_shift = 3;
 
 # This is binary flag (0 or 1) specifying whether to suppress (1) or
 # allow (0) the console's text updates when each image is saved.
@@ -57,41 +57,183 @@ bit_shift = 4;
 # The default setting is to not suppress messages, i.e., suppress_messages = 0;
 suppress_messages = 0;
 
-# Animal numbers
-animal_nums = list(range(7,8))
 
-for k in animal_nums:
-    animal_name = "grasshopper_" + str(k)
-    animal_dir = os.path.join(data_input_parent_dir, animal_name)
-    
-    # Get all the names of the dirs in this folder
-    trial_names = os.listdir(animal_dir)
-    
-    # Loop over this directory
-    for m in trial_names:
-        if not ".DS" in m:
-            # Specity the data input directory
-            data_input_dir = os.path.join(animal_dir, m)
-            
-            # Specify the output data directory
-            data_output_dir = os.path.join(data_output_parent_dir, animal_name, m)
-            
-            # Make the dir if it doesn't already exist
-            os.makedirs(data_output_dir, exist_ok=True)
-            
-            # Specify the CIH and mraw file names to be the same as the trial name.
-            cih_file_name = m + ".cih"
-            mraw_file_name = m + ".mraw"
-            
-            # Some printing
-            print("CIH file name: " + cih_file_name)
-            print("mraw file name: " + mraw_file_name)
-            
-            # Run the python wrapper.
-            mraw2tiff(data_input_dir = data_input_dir, cih_file_name = cih_file_name, mraw_file_name = mraw_file_name, data_output_dir = data_output_dir, start_image = start_image, end_image = end_image, bit_shift = bit_shift, exec_path = exec_path, suppress_messages=suppress_messages);
 
-            
-            
+# Here is where we specify the list of cases to export.
+animal_nums = [];
+animal_names = [];
+
+# Repeat this block for each case
+#
+# 
+
+##############
+##############
+## ANIMAL 1 ##
+##############
+##############
+
+# # #
+animal_num = 1;
+animal_name = 'mng_2-071-C';
+animal_nums.append(animal_num);
+animal_names.append(animal_name);
+
+# # #
+animal_num = 1;
+animal_name = 'mng_2_071-E';
+animal_nums.append(animal_num);
+animal_names.append(animal_name);
+
+
+
+##############
+##############
+## ANIMAL 2 ##
+##############
+##############
+
+# # # #
+animal_num = 2;
+animal_name = 'mng_2-071-J';
+animal_nums.append(animal_num);
+animal_names.append(animal_name);
+
+
+
+##############
+##############
+## ANIMAL 3 ##
+##############
+##############
+
+# # # #
+animal_num = 3;
+animal_name = 'mng_2_072_B';
+animal_nums.append(animal_num);
+animal_names.append(animal_name);
+
+# # # #
+animal_num = 3;
+animal_name = 'mng_2_072_C';
+animal_nums.append(animal_num);
+animal_names.append(animal_name);
+
+# # # #
+animal_num = 3;
+animal_name = 'mng_2_072_D';
+animal_nums.append(animal_num);
+animal_names.append(animal_name);
+
+
+
+##############
+##############
+## ANIMAL 4 ##
+##############
+##############
+
+# # # #
+animal_num = 4;
+animal_name = 'mng_2-072-F';
+animal_nums.append(animal_num);
+animal_names.append(animal_name);
+
+
+# # # #
+animal_num = 4;
+animal_name = 'mng_2-072-G';
+animal_nums.append(animal_num);
+animal_names.append(animal_name);
+
+
+
+
+##############
+##############
+## ANIMAL 5 ##
+##############
+##############
+
+# # # #
+animal_num = 5;
+animal_name = 'mng_2-073-C';
+animal_nums.append(animal_num);
+animal_names.append(animal_name);
+
+
+
+
+##############
+##############
+## ANIMAL 6 ##
+##############
+##############
+
+# # # #
+animal_num = 6;
+animal_name = 'mng_2-073-F';
+animal_nums.append(animal_num);
+animal_names.append(animal_name);
+
+# # # #
+animal_num = 6;
+animal_name = 'mng_2-073-G';
+animal_nums.append(animal_num);
+animal_names.append(animal_name);
+
+
+
+
+##############
+##############
+## ANIMAL 7 ##
+##############
+##############
+
+# # # #
+animal_num = 7;
+animal_name = 'mng_2-073-L';
+animal_nums.append(animal_num);
+animal_names.append(animal_name);
+
+# # # #
+animal_num = 7;
+animal_name = 'mng_2-073-M';
+animal_nums.append(animal_num);
+animal_names.append(animal_name);
+
+
+# for k in animal_nums:
+#     animal_name = "grasshopper_" + str(k)
+#     animal_dir = os.path.join(data_input_parent_dir, animal_name)
+#
+#     # Get all the names of the dirs in this folder
+#     trial_names = os.listdir(animal_dir)
+#
+#     # Loop over this directory
+#     for m in trial_names:
+#         if not ".DS" in m:
+#             # Specity the data input directory
+#             data_input_dir = os.path.join(animal_dir, m)
+#
+#             # Specify the output data directory
+#             data_output_dir = os.path.join(data_output_parent_dir, animal_name, m)
+#
+#             # Make the dir if it doesn't already exist
+#             os.makedirs(data_output_dir, exist_ok=True)
+#
+#             # Specify the CIH and mraw file names to be the same as the trial name.
+#             cih_file_name = m + ".cih"
+#             mraw_file_name = m + ".mraw"
+#
+#             # Some printing
+#             print("CIH file name: " + cih_file_name)
+#             print("mraw file name: " + mraw_file_name)
+#
+#             # Run the python wrapper.
+#             mraw2tiff(data_input_dir = data_input_dir, cih_file_name = cih_file_name, mraw_file_name = mraw_file_name, data_output_dir = data_output_dir, start_image = start_image, end_image = end_image, bit_shift = bit_shift, exec_path = exec_path, suppress_messages=suppress_messages);
+#
             
 #
 # #
